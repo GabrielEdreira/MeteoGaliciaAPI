acd16da5127e9abd4287b18e975fea0927ad1a79
GabrielEdreira <gabrieledreira@gmail.com>
2026-04-29 19:31:33 +0200

    Configuración de ejemlos en proyecto

--- MeteoGaliciaAPI/Program.cs
+++ MeteoGaliciaAPI/Program.cs
@@ -4,8 +4,6 @@ using MeteoGaliciaAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
